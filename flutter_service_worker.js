481a2f2b7a24c8b5393979d2b88bcf5be5d20b8e
CrewZApp <99058268+CrewZApp@users.noreply.github.com>
2025-05-21 13:02:59 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3898ca647e08361b7f189bce8a42e7fb",
+const RESOURCES = {"flutter_bootstrap.js": "464cc8ba3a3d8a640fdaaa0c9e91a49e",
 "version.json": "04fc0d353cb8d9ad0b36fc72596aa60e",
 "index.html": "5eddbfbc417a91bb1b40d62494e8dfaf",
 "/": "5eddbfbc417a91bb1b40d62494e8dfaf",
 "vercel.json": "6411d2fbb5f0e00aaff0ca0ded84e0b6",
-"main.dart.js": "650adcd6561093f3af9fae19d79d9ec1",
+"main.dart.js": "c801bec45cbd0e9c1b95c68ec73c129d",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
