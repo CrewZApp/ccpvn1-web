d2ab97eefc59eeb3278c38780649a3dc25de210b
CrewZApp <99058268+CrewZApp@users.noreply.github.com>
2025-05-23 16:27:43 -0400

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1714084483"
+    serviceWorkerVersion: "1947810584"
   }
 });
